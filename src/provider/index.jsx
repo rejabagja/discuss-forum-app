c7c2be334bcd57666d06563024994d60b8c56a4a
Reza Subagja <rezasubagja5@gmail.com>
2025-04-19 11:30:07 +0700

    Add PropTypes validation for AppProvider component

--- src/provider/index.jsx
+++ src/provider/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '@states/index';
+import PropTypes from 'prop-types';
 
 const AppProvider = ({ children }) => {
   return (
@@ -8,6 +9,10 @@ const AppProvider = ({ children }) => {
       <BrowserRouter>{children}</BrowserRouter>
     </Provider>
   );
+};
+
+AppProvider.propTypes = {
+  children: PropTypes.node,
 };
 
 export default AppProvider;
