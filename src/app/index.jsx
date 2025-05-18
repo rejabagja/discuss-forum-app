ebbe56bfe3cb658afae81699c2463dcfc4baa640
Reza Subagja <rezasubagja5@gmail.com>
2025-05-18 10:53:21 +0700

    move preload process into App component

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,6 +1,37 @@
 import AppRoutes from '@routes';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { preloadProcess } from '@states/thunks';
+import PageOffline from '@components/PageOffline';
 
 function App() {
+  const dispatch = useDispatch();
+  const preload = useSelector(({ preload }) => preload);
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    dispatch(preloadProcess());
+  }, [dispatch]);
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
+
+  if (preload) return null;
+
+  if (!isOnline) {
+    return <PageOffline />;
+  }
+
   return <AppRoutes />;
 }
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { preloadProcess } from '@states/thunks';
 import LayoutApp from '@layouts/App';
 import PageNotFound from '@pages/not-found';
 import PageThreadCreate from '@pages/thread-create';
@@ -11,36 +8,8 @@ import AuthRoutes from './AuthRoutes';
 
 import RequireAuth from './guards/RequireAuth';
 import GuestOnly from './guards/GuestOnly';
-import PageOffline from '@components/PageOffline';
 
 const AppRoutes = () => {
-  const preload = useSelector(({ preload }) => preload);
-  const dispatch = useDispatch();
-  const [isOnline, setIsOnline] = useState(navigator.onLine);
-
-  useEffect(() => {
-    dispatch(preloadProcess());
-  }, [dispatch]);
-
-  useEffect(() => {
-    const handleOnline = () => setIsOnline(true);
-    const handleOffline = () => setIsOnline(false);
-
-    window.addEventListener('online', handleOnline);
-    window.addEventListener('offline', handleOffline);
-
-    return () => {
-      window.removeEventListener('online', handleOnline);
-      window.removeEventListener('offline', handleOffline);
-    };
-  }, []);
-
-  if (preload) return null;
-
-  if (!isOnline) {
-    return <PageOffline />;
-  }
-
   return (
     <Routes>
       <Route element={<LayoutApp />}>
