64295b58310aee6955ce19152ec7fc24e0f8875e
rejabagja <rezasubagja5@gmail.com>
2025-05-29 21:26:07 +0700

    add heading on AuthPageWrapper component to see behaviour ci:test

--- src/layouts/AuthPageWrapper.jsx
+++ src/layouts/AuthPageWrapper.jsx
@@ -5,6 +5,7 @@ const AuthPageWrapper = ({ title, subtitle, children, linkAction }) => {
   return (
     <section className="auth-page flex-1 flex flex-col justify-center">
       <div className="auth-page__content max-w-sm w-full mx-auto">
+        <h1 className="text-3xl text-primary font-bold">Discuss</h1>
         <h2 className="font-semibold text-2xl mb-2">{title}</h2>
         <p className="mb-4">{subtitle}</p>
         {children}
