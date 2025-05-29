49321745223bd6140d70e1fab67dabdb01217efd
rejabagja <rezasubagja5@gmail.com>
2025-05-29 12:33:17 +0700

    feat(e2e): integrate Cypress for end-to-end testing
    - Configure Cypress and set up initial project structure for E2E tests
    - Add ESLint plugin for Cypress to enforce best practices in test writing
    
    This setup enables robust end-to-end testing to ensure the application behaves correctly from a user perspective.

---
+++ cypress/e2e/create_thread.cy.js
@@ -0,0 +1,75 @@
+/*
+create thread spec:
+- should display create thread page correctly
+- should shown error message when title field is empty
+- should shown error message when body field is empty
+- should show teks 'Creating...' on submit button when create thread is processing
+- should redirect to home page when create thread success
+*/
+
+describe('Create Thread Spec', () => {
+  const hostname = 'http://localhost:5173';
+  const username = 'testuser2';
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzZXItU2o5eVhpQUp5MTBLRlBmNiIsImlhdCI6MTc0ODQ5MjAyN30.Wnp4Rm2BHEodyg3KX1OAVTWsETYdvjq-Cxp7fNEzugQ';
+
+  beforeEach(() => {
+    cy.visit(`${hostname}/`, {
+      onBeforeLoad(win) {
+        win.localStorage.setItem('token', token);
+      },
+      onLoad() {
+        cy.get('a[href="/create"]').click();
+      }
+    });
+  });
+
+  it('should display create thread page correctly', () => {
+    cy.contains('h2', 'Create New Thread').should('be.visible');
+    cy.get('input[placeholder="Title"]').should('be.visible');
+    cy.get('input[placeholder="Category"]').should('be.visible');
+    cy.contains('Write your thread body here...').should('be.visible');
+    cy.get('div[contenteditable="true"]').should('be.visible');
+    cy.contains('button[type="button"]', /^Create$/).should('be.visible');
+  });
+
+  it('should shown error message when title field is empty', () => {
+    cy.get('div[contenteditable="true"]').type('test body');
+    cy.contains('button[type="button"]', /^Create$/).click();
+
+    cy.contains('p.text-red-500', '"title" is not allowed to be empty').should('be.visible');
+  });
+
+  it('should shown error message when body field is empty', () => {
+    cy.get('input[placeholder="Title"]').type('test title');
+    cy.contains('button[type="button"]', /^Create$/).click();
+
+    cy.contains('p.text-red-500', '"body" is not allowed to be empty').should('be.visible');
+  });
+
+  it('should show teks "Creating..." on submit button when create thread is processing', () => {
+    // create thread req intercept
+    cy.intercept('POST', 'https://forum-api.dicoding.dev/v1/threads', (req) => {
+      req.on('response', (res) => {
+        res.setDelay(3000);
+      });
+    }).as('createThreadRequest');
+
+    cy.get('input[placeholder="Title"]').type('test title');
+    cy.contains('button[type="button"]', /^Create$/).click();
+
+    cy.contains('button[type="button"]', /^Creating...$/).should('be.visible');
+
+    cy.wait('@createThreadRequest');
+
+    cy.contains(/^Creating...$/).should('not.exist');
+  });
+
+  it('should redirect to home page when create thread success', () => {
+    cy.get('input[placeholder="Title"]').type('test title');
+    cy.get('input[placeholder="Category"]').type('test category');
+    cy.get('div[contenteditable="true"]').type('test body');
+    cy.contains('button[type="button"]', /^Create$/).click();
+    cy.url().should('eq', 'http://localhost:5173/');
+  });
+});
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,100 @@
+/*
+login spec:
+- should display login page correctly
+- should shown HTML form validation when fields are empty
+- should shown HTML form validation when email field is invalid
+- should show teks 'Logging in...' on submit button when login is processing
+- should show error message when login failed
+- should redirect to home page when login success and should have an user avatar on navbar
+*/
+
+describe('login spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/login');
+  });
+
+  it('should display login page correctly', () => {
+    // verify layout app ui
+    cy.contains('header h1', 'Discuss Forum App').should('be.visible');
+    cy.get('footer nav')
+      .should('be.visible')
+      .within(() => {
+        cy.get('a[href="/"]').contains('Threads').should('be.visible');
+        cy.get('a[href="/leaderboards"]').contains('Leaderboards').should('be.visible');
+        cy.get('a[href="/login"]').contains('Login').should('be.visible');
+      });
+
+
+    // verify login page ui
+    cy.contains('h2', 'Log Into Your Account').should('be.visible');
+    cy.contains('p', 'Access the forum and join the discussion.').should('be.visible');
+    cy.get('input[type="email"][placeholder="Email"]').should('be.visible');
+    cy.get('input[type="password"][placeholder="Password"]').should('be.visible');
+    cy.contains('button', /^Login$/).should('be.visible');
+    cy.contains('p', 'Don\'t have an account?')
+      .should('be.visible')
+      .within(() => {
+        cy.get('a[href="/register"]').contains('register').should('be.visible');
+      });
+  });
+
+  it('should shown HTML form validation when fields are empty', () => {
+    // fields are empty
+    cy.contains('button[type="submit"]', /^Login$/).click();
+    cy.get('input:invalid').should('have.length', 2);
+    cy.get('input').should(($input) => {
+      expect($input[0].validationMessage).to.eq('Please fill out this field.');
+      expect($input[1].validationMessage).to.eq('Please fill out this field.');
+    });
+  });
+
+  it('should shown HTML form validation when email field is invalid', () => {
+    cy.get('input[type="email"]').type('invalid-email');
+    cy.get('input[type="password"]').type('password');
+    cy.contains('button[type="submit"]', /^Login$/).click();
+    cy.get('input:invalid').should('have.length', 1);
+    cy.get('input:invalid').then(($input) => {
+      const message = $input[0].validationMessage;
+      expect(message).to.match(/@|email/i);
+    });
+  });
+
+  it('should show teks "Logging in..." on submit button when login is processing', () => {
+    // login req intercept
+    cy.intercept('POST', 'https://forum-api.dicoding.dev/v1/login', (req) => {
+      req.on('response', (res) => {
+        res.setDelay(3000);
+      });
+    }).as('loginRequest');
+
+    cy.get('input[type="email"]').type('testy@mail.com');
+    cy.get('input[type="password"]').type('password');
+    cy.contains('button[type="submit"]', /^Login$/).click();
+
+    cy.contains('button[type="submit"]', /^Logging in...$/).should(
+      'be.visible'
+    );
+
+    cy.wait('@loginRequest');
+
+    cy.contains(/^Logging in...$/).should('not.exist');
+  });
+
+  it('should show error message when login failed', () => {
+    cy.get('input[type="email"]').type('testy@mail.com');
+    cy.get('input[type="password"]').type('password');
+    cy.contains('button[type="submit"]', /^Login$/).click();
+    cy.contains('p.text-red-500', 'email or password is wrong').should('be.visible');
+  });
+
+  it('should redirect to home page when login success and have an user avatar on navbar', () => {
+    const username = 'testuser2';
+    const email = 'testy@mail.com';
+    const password = '123456';
+    cy.get('input[type="email"]').type(email);
+    cy.get('input[type="password"]').type(password);
+    cy.contains('button[type="submit"]', /^Login$/).click();
+    cy.url().should('eq', 'http://localhost:5173/');
+    cy.get(`.avatar img[alt="${username}"]`).should('be.visible');
+  });
+});
